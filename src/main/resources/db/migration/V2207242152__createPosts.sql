adce1c255a84c4c23779d074cad9ab1b55e1830b
LAPTOP-PIPPGDVR\akira pc <enuz0129@gmail.com>
2022-07-26 22:56:32 +0900

    idのデータ型serial、列変更

--- src/main/resources/db/migration/V2207242152__createPosts.sql
+++ src/main/resources/db/migration/V2207242152__createPosts.sql
@@ -1,16 +1,13 @@
 CREATE TABLE posts (
-id sequence,
-user_id int NOT NULL,
-post_id int NOT NULL,
+id serial,
+user_id int,
 content text NOT NULL,
-imageUrls text,
-videoUrls text,
 postedAt timestamp,
 postType int,
 CONSTRAINT posts_id_pk PRIMARY KEY(id),
 CONSTRAINT users_id_fk FOREIGN KEY (user_id) REFERENCES users (id)
     ON UPDATE CASCADE
-    ON DELETE CASCADE
+    ON DELETE SET NULL
 );
 
 
--- src/main/resources/db/migration/V2207242250__createAttachedMediaContents.sql
+++ src/main/resources/db/migration/V2207242250__createAttachedMediaContents.sql
@@ -1,9 +1,9 @@
 CREATE TABLE attached_media_contents (
-id sequence,
+id serial,
 post_id int NOT NULL,
 url text,
 mediaType int,
-CONSTRAINT amc_id_pk PRIMARY KEY(id)
+CONSTRAINT amc_id_pk PRIMARY KEY(id),
 CONSTRAINT posts_id_fk FOREIGN KEY (post_id) REFERENCES posts (id)
     ON UPDATE CASCADE
     ON DELETE CASCADE
